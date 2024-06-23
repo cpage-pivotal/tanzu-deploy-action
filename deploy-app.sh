810d7bd516cb8ea952b51e1a7bc320276405ba8d
Corby <pcorby@vmware.com>
2024-06-23 16:26:32 -0500

    Added build plan source input

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,4 +1,10 @@
 tanzu build config --build-plan-source-type=ucp --containerapp-registry $CONTAINER_REGISTRY
+if [[ -z "${BUILD_PLAN_SOURCE}" ]]; then
+  tanzu build config --build-plan-source-type=ucp --containerapp-registry $CONTAINER_REGISTRY
+else
+  tanzu build config --build-plan-source-type=ucp --containerapp-registry $CONTAINER_REGISTRY --build-plan-source $BUILD_PLAN_SOURCE
+fi
+
 tanzu project use $PROJECT
 tanzu space use $SPACE
 
