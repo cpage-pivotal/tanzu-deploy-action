be2ea3d186d787ed1d2f05874816a02411c6c55f
Corby <pcorby@vmware.com>
2024-06-25 19:58:36 -0500

    Added docs for build plan source

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,7 +1,7 @@
 if [[ -z "${BUILD_PLAN_SOURCE}" ]]; then
-  tanzu build config --build-plan-source-type=ucp --containerapp-registry $CONTAINER_REGISTRY
+  tanzu build config --build-plan-source-type=ucp --containerapp-registry ${CONTAINER_REGISTRY@L}
 else
-  tanzu build config --build-plan-source-type=ucp --containerapp-registry $CONTAINER_REGISTRY --build-plan-source $BUILD_PLAN_SOURCE
+  tanzu build config --build-plan-source-type=ucp --containerapp-registry ${CONTAINER_REGISTRY@L} --build-plan-source $BUILD_PLAN_SOURCE
 fi
 
 tanzu project use $PROJECT
