1edb97d204ebef6cb072742a29f0fb0d4e40a36c
Corby <pcorby@vmware.com>
2024-07-12 16:16:51 -0500

    Add JAVA_VERSION parameter. Provide default for BUILD_PLAN_SOURCE

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,8 +1,5 @@
-if [[ -z "${BUILD_PLAN_SOURCE}" ]]; then
-  tanzu build config --build-plan-source-type=ucp --containerapp-registry ${CONTAINER_REGISTRY@L}
-else
-  tanzu build config --build-plan-source-type=ucp --containerapp-registry ${CONTAINER_REGISTRY@L} --build-plan-source $BUILD_PLAN_SOURCE
-fi
+tanzu build config --build-plan-source-type=ucp --containerapp-registry ${CONTAINER_REGISTRY@L} --build-plan-source $BUILD_PLAN_SOURCE
+tanzu app config build non-secret-env set --app=spring-music BP_JVM_VERSION=${JAVA_VERSION}
 
 tanzu project use $PROJECT
 tanzu space use $SPACE
