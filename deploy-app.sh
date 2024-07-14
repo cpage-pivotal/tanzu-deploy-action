268776375049945c48950b5b244ca2cd6ac0e403
Corby <pcorby@vmware.com>
2024-07-14 13:00:10 -0500

    Troubleshooting build config

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,5 @@
 tanzu build config --build-plan-source-type=ucp --containerapp-registry ${CONTAINER_REGISTRY@L} --build-plan-source $BUILD_PLAN_SOURCE
-tanzu app config build non-secret-env set --app=spring-music BP_JVM_VERSION=${JAVA_VERSION}
+tanzu app config build non-secret-env set BP_JVM_VERSION=${JAVA_VERSION}
 
 tanzu project use $PROJECT
 tanzu space use $SPACE
