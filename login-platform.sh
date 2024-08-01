b4eda4deccc3ca6a6e5eb7f19eff4271ebb16efd
Corby <pcorby@vmware.com>
2024-08-01 10:51:24 -0500

    Upgrade to 1.4

--- login-platform.sh
+++ login-platform.sh
@@ -1,7 +1,7 @@
 export TANZU_CLI_CEIP_OPT_IN_PROMPT_ANSWER=no
-wget https://github.com/vmware-tanzu/tanzu-cli/releases/download/v1.3.0/tanzu-cli-linux-amd64.tar.gz -O /tmp/tanzu.tar.gz
+wget https://github.com/vmware-tanzu/tanzu-cli/releases/download/v1.4.0/tanzu-plugins-admin-linux-amd64.tar.gz -O /tmp/tanzu.tar.gz
 tar -xvf /tmp/tanzu.tar.gz -C /tmp
-install /tmp/v1.3.0/tanzu-cli-linux_amd64 /usr/local/bin/tanzu
+install /tmp/v1.4.0/tanzu-cli-linux_amd64 /usr/local/bin/tanzu
 tanzu config eula accept
 tanzu plugin install --group vmware-tanzu/app-developer
 
