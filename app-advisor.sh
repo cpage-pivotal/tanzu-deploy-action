1398485c10438fc84d9fb3e7d227ed15d4905ec2
Corby <pcorby@vmware.com>
2024-08-10 18:44:33 -0500

    fixes

--- app-advisor.sh
+++ app-advisor.sh
@@ -1,4 +1,4 @@
-curl -L -H "Authorization: Bearer $ARTIFACTORY_TOKEN" -o advisor-cli.tar -X GET https://packages.broadcom.com/artifactory/spring-enterprise/com/vmware/tanzu/spring/application-advisor-cli-linux/0.0.8/application-advisor-cli-linux-0.0.9.tar
+curl -L -H "Authorization: Bearer $ARTIFACTORY_TOKEN" -o advisor-cli.tar -X GET https://packages.broadcom.com/artifactory/spring-enterprise/com/vmware/tanzu/spring/application-advisor-cli-linux/0.0.9/application-advisor-cli-linux-0.0.9.tar
 tar -xf advisor-cli.tar --strip-components=1 --exclude=./META-INF -C /tmp
 install /tmp/advisor /usr/local/bin/advisor
 
